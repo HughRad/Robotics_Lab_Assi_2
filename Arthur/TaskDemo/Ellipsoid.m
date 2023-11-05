c1c2a1da23908acd7371ab0c4ef408ed4d78b2ea
CrrisC <145747056+CrrisC@users.noreply.github.com>
2023-11-05 17:38:16 +1100

    Update Ellipsoid.m
    Comments

--- Arthur/TaskDemo/Ellipsoid.m
+++ Arthur/TaskDemo/Ellipsoid.m
@@ -14,38 +14,53 @@ classdef Ellipsoid < handle
     end
 
     methods
+    %Takes each robot in as a parameter, as well as the vertices of
+    %potential collisions
+    %Initialising parameters for use in collision functions
         function obj = Ellipsoid(UR3,CR5,tapvert)
             obj.UR3robot = UR3;
             obj.CR5robot = CR5;
-
+            
+        %setting UR3 & CR5 transforms, acounting for links
             obj.tranUR3 = zeros(4, 4, obj.UR3robot.n + 1);
             obj.tranCR5 = zeros(4, 4, obj.CR5robot.n + 1);
-
+            
+        %acounting for UR3 & CR5 base transforms
             obj.tranUR3(:,:,1) = obj.UR3robot.base;
             obj.tranCR5(:,:,1) = obj.CR5robot.base;
 
+        %mesh size for ellipsoid generation for CR5
             obj.CR5meshSize = [0.07,0.23,0.09;0.07,0.2,0.07;...
                         0.07,0.14,0.07;0.07,0.14,0.07;0.07,0.07,0.07];
-
+                        
+        %mesh offset for links in ellipsoid generation for CR5
             obj.CR5meshOffset = [0,0,0.09;0,0,-0.07;...
                         0,0,-0.03;0,0,0;0,0,0];
-            
+                        
+        %mesh size for ellipsoid generation for UR3 
             obj.URmeshSize = [0.2, 0.06, 0.09; 0.18, 0.06, 0.06;...
                 0.06, 0.06, 0.06; 0.06, 0.1, 0.06; 0.06, 0.06, 0.06];
-
+                
+        %mesh offset for links in ellipsoid generation for UR3
             obj.URmeshOffset = [0, 0, 0.12; 0, 0, 0; 0, 0, 0; 0, 0, 0; 0, 0, 0];
 
+        %obtain link information for each robot
             obj.CR5Links = CR5.links;
             obj.UR3Links = UR3.links;
-
+            
+        %obtain vertice information for potential collisions
             obj.tapvert = tapvert;
         end
 
+        %creates and attaches the ellipsoid meshes to the UR3 based on its current joint position
         function collision = URmesh(obj, q)
+        
+        %creates transforms for each link in the robot based off respective parameters
            for i = 1:obj.UR3robot.n
                 obj.tranUR3(:,:,i+1) = obj.tranUR3(:,:,i)*trotz(q(i))*transl(obj.UR3Links(i).a,0,obj.UR3Links(i).d)*trotx(obj.UR3Links(i).alpha);
            end
 
+        %creates transform for the center of each link
            tranCenter = obj.tranUR3;
 
            for i = 1:5
@@ -55,13 +70,16 @@ classdef Ellipsoid < handle
                 elippoints = [x(:), y(:), z(:)]';
                 TranElip = tranCenter(:,:,i+2) * [elippoints; ones(1, size(elippoints,2))];
                 TranElip = TranElip(1:3,:);
+
+                %displays ellipsoid in environment as mesh for troubleshooting
                 % x1 = reshape(TranElip(1,:), size(x));
                 % y1 = reshape(TranElip(2,:), size(y));
                 % z1 = reshape(TranElip(3,:), size(z));
                 % surf(x1, y1, z1);
 
                 MeshTriang = delaunayTriangulation(unique(TranElip', 'rows'));
-                
+
+                %checks if any vertice is located in the robots ellipsoid collision mesh
                 for p = 1:1:size(obj.tapvert,1)
                     ID = pointLocation(MeshTriang,obj.tapvert(p,:));
                     if ~isnan(ID)
@@ -74,8 +92,11 @@ classdef Ellipsoid < handle
            collision = false;
            return;
         end
-
+        
+        %creates and attaches the ellipsoid meshes to the CR5 based on its current joint position
         function collision = CR5mesh(obj, q)
+
+        %creates transforms for each link in the robot based off respective parameters
             for i = 1:obj.CR5robot.n
                 if i == 2 || i == 3
                     obj.tranCR5(:,:,i+1) = obj.tranCR5(:,:,i)*trotz(q(i))*transl(0,abs(obj.CR5Links(i).a),obj.CR5Links(i).d)*trotx(obj.CR5Links(i).alpha); 
@@ -85,15 +106,19 @@ classdef Ellipsoid < handle
                     obj.tranCR5(:,:,i+1) = obj.tranCR5(:,:,i)*trotz(q(i))*transl(-obj.CR5Links(i).a,0,-obj.CR5Links(i).d)*trotx(obj.CR5Links(i).alpha); 
                 end
             end
-        
+            
+        %creates transform for the center of each link
             tranCenter = obj.tranCR5;
 
             for i = 1:5
+             % Create and display ellipsoids for each link
                 tranCenter(1:3,4,i+2) = obj.tranCR5(1:3,4,i+1)+((obj.tranCR5(1:3,4,i+2) - obj.tranCR5(1:3,4,i+1))/2);
                 [x, y, z] = ellipsoid(obj.CR5meshOffset(i,1), obj.CR5meshOffset(i,2), obj.CR5meshOffset(i,3), obj.CR5meshSize(i,1), obj.CR5meshSize(i,2), obj.CR5meshSize(i,3),5);
                 elippoints = [x(:), y(:), z(:)]';
                 TranElip = tranCenter(:,:,i+2) * [elippoints; ones(1, size(elippoints,2))];
                 TranElip = TranElip(1:3,:);
+
+                %displays ellipsoid in environment as mesh for troubleshooting 
                 % x1 = reshape(TranElip(1,:), size(x));
                 % y1 = reshape(TranElip(2,:), size(y));
                 % z1 = reshape(TranElip(3,:), size(z));
@@ -101,6 +126,7 @@ classdef Ellipsoid < handle
 
                 MeshTriang = delaunayTriangulation(unique(TranElip', 'rows'));
 
+             %checks if any vertice is located in the robots ellipsoid collision mesh
                 for p = 1:1:size(obj.tapvert,1)
                     ID = pointLocation(MeshTriang,obj.tapvert(p,:));
                     if ~isnan(ID)
